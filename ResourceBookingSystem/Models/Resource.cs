6154548f5eedb7a8d34e33c9b9eeafe25a66b5ac
Lihle Magidigidi <lihlethornmagidigidi@gmail.com>
2025-12-03 17:18:26 +0200

    Added Resource model with basic properties and bookings navigation

---
+++ ResourceBookingSystem/Models/Resource.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceBookingSystem.Models
+{
+    public class Resource
+    {
+        public int Id { get; set; }  // Primary key
+
+        [Required]
+        public string Name { get; set; } = "";  // Name of the resource
+
+        public string Description { get; set; } = "";  // Optional description
+        public string Location { get; set; } = "";     // Location of the resource
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Capacity { get; set; }  // Maximum capacity
+
+        public bool IsAvailable { get; set; } = true;  // Availability status
+
+        // List of bookings for this resource
+        public List<Booking> Bookings { get; set; } = new();
+    }
+}
